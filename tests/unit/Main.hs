a5dea4e17e4710b03ea913cb89717b367dd599ba
jkachmar <j@mercury.com>
2026-05-26 01:47:09 -0400

    feat: parallel test suite
    now that 'Session' has been refactored we can run the test suite in
    parallel.

--- tests/unit/Main.hs
+++ tests/unit/Main.hs
@@ -5,16 +5,13 @@ import Control.Exception (bracket_)
 import GeneratorSchemas qualified
 import Hegel (closeSession, globalSession)
 import SessionRecovery qualified
-import Test.Tasty (defaultMain, localOption, testGroup)
+import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.Hspec (testSpec)
-import Test.Tasty.Runners (NumThreads (..))
 
 main :: IO ()
 main = do
   basics <- testSpec "basic properties" BasicProperties.spec
   schemas <- testSpec "generator schemas" GeneratorSchemas.spec
   recovery <- testSpec "session recovery" SessionRecovery.spec
-  let tree =
-        localOption (NumThreads 1) $
-          testGroup "zizek:unit" [basics, schemas, recovery]
+  let tree = testGroup "zizek:unit" [basics, schemas, recovery]
   bracket_ (pure ()) (closeSession globalSession) (defaultMain tree)
